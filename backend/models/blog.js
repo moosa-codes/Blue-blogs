c9fed6125a9e2af5798f45a17e94da4fcaa4d756
Moosa Haroon <moosaharoon6nov@gmail.com>
2025-03-29 06:30:13 +0500

    models

---
+++ backend/models/blog.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+    img: {
+        type: String,
+        required: true,
+    },
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+const Blog = mongoose.model("Blog", blogSchema);
+export default Blog;
